a78e877b701b73a08140a5754145b6c376d43dfd
dvxi <sim28888@gmail.com>
2022-10-14 20:24:06 +0200

    login screen, card component, navigation

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 import { NavigationContainer } from '@react-navigation/native';
@@ -26,6 +27,7 @@ export default function App() {
   return (
     <NavigationContainer>
       {Navigator}
+      <StatusBar style="light" />
     </NavigationContainer>
   );
 }
--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -1,9 +1,25 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
+
+import Card from '../components/Card';
 
 const Login = () => {
   return (
     <View style={styles.container}>
-      <Text style={ styles.text }>Testowy tekst</Text>
+      <Card title="Wpisz swoje imię" content="Wyświetli się ono na ekranie, gdy nadejdzie kolej na Twoje ciastko" input />
+      <Card title="Wpisz swoje imię" content="Wyświetli się ono na ekranie, gdy nadejdzie kolej na Twoje ciastko" input />
+      <View style={styles.subcontainer}>
+        <Text style={styles.textTitle}>
+          Gotowy?
+        </Text>
+        <Text style={styles.textSecondary}>
+          Gdy wykonasz wszystkie powyższe instrukcje przycisk poniżej zmieni się na aktywny.
+        </Text>
+      </View>
+      <View style={styles.subcontainer}>
+        <Pressable style={styles.button.main}>
+          <Text style={styles.textMedium}>Przejdź do projektowania</Text>
+        </Pressable>
+      </View>
     </View>
   )
 };
@@ -14,9 +30,37 @@ const styles = StyleSheet.create({
     backgroundColor: '#1c1c1e',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingHorizontal: 30,
   },
-  text: {
-    color: '#fff'
+  subcontainer: {
+    alignItems: 'center',
+    marginVertical: 25,
+  },
+  textMedium: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold'
+  },
+  textTitle: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 19,
+    marginVertical: 5,
+  },
+  textSecondary: {
+    color: "#8E8E93",
+    fontSize: 14,
+    marginVertical: 5,
+  },
+  button: {
+    main:{
+      backgroundColor: '#FF8854',
+      padding: 20,
+      borderRadius: 15
+    },
+    secondary: {
+
+    }
   }
 });
 
