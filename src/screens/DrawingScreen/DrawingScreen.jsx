d1b1ee5df1881bb185d3c73fce72e94c08ced16e
jrh-w <jakub.r.wlodarz@gmail.com>
2022-10-27 22:39:59 +0200

    Implement basic drawing board

--- src/screens/DrawingScreen/DrawingScreen.jsx
+++ src/screens/DrawingScreen/DrawingScreen.jsx
@@ -1,10 +1,97 @@
-import { Text, View } from 'react-native';
+import { useCallback, useRef, useState } from 'react';
+import { SafeAreaView, View } from 'react-native';
+import { Canvas, Skia, SkiaView, Path, useDrawCallback, useTouchHandler } from '@shopify/react-native-skia';
+import styles from './DrawingScreen.style';
 
 const Drawing = () => {
+  const touchState = useRef(false);
+  const canvas = useRef();
+  const currentPath = useRef();
+  const [completedPaths, setCompletedPaths] = useState([]);
+
+  const updatePaths = () => {
+    if (!currentPath.current) return;
+    let updatedPaths = {
+      path: currentPath.current?.path.copy(),
+      paint: currentPath.current?.paint.copy()
+    };
+    setCompletedPaths(completedPaths => [...completedPaths, updatedPaths]);
+  };
+
+  const onDrawingActive = useCallback((touchInfo) => {
+    const {x, y} = touchInfo;
+    if (!currentPath.current?.path) return;
+    if (touchState.current) {
+      currentPath.current.path.lineTo(x, y);
+      if (currentPath.current) {
+        canvas.current?.drawPath(
+          currentPath.current.path,
+          currentPath.current.paint,
+        );
+      }
+    }
+  }, []);
+
+  const onDrawingStart = useCallback(
+    (touchInfo) => {
+      if (currentPath.current) return;
+      const { x, y } = touchInfo;
+
+      const paint = Skia.Paint();
+      paint.setColor(Skia.Color("black"));
+      paint.setStyle(1);
+      paint.setStrokeWidth(5);
+      
+      currentPath.current = {
+        path: Skia.Path.Make(),
+        paint
+      };
+
+      touchState.current = true;
+      currentPath.current.path?.moveTo(x, y);
+
+      if (currentPath.current) {
+        canvas.current?.drawPath(currentPath.current.path, currentPath.current.paint);
+      }
+    },
+    [],
+  );
+
+  const onDrawingFinished = useCallback(() => {
+    updatePaths();
+    currentPath.current = null;
+    touchState.current = false;
+  }, []);
+
+  const touchHandler = useTouchHandler({
+    onActive: onDrawingActive,
+    onStart: onDrawingStart,
+    onEnd: onDrawingFinished,
+  });
+
+  const onDraw = useDrawCallback((_canvas, info) => {
+    touchHandler(info.touches);
+    canvas.current = _canvas;
+  }, []);
+
   return (
-    <View>
-        <Text>Hello</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+        <View style={styles.canvasContainer}>
+          <SkiaView
+            onDraw={onDraw}
+            style={styles.drawingBoard}
+          />
+          <Canvas style={styles.canvas}>
+            {completedPaths?.map((path) => (
+              <Path
+                key={path.path.toSVGString()}
+                path={path.path}
+                paint={{ current: path.paint }}
+              />
+            ))}
+          </Canvas>
+        </View>
+    </SafeAreaView>
   )
 };
 
--- src/screens/DrawingScreen/DrawingScreen.style.jsx
+++ src/screens/DrawingScreen/DrawingScreen.style.jsx
@@ -1,5 +1,27 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
-    
+    container: {
+      flex: 1,
+      justifyContent: 'center'
+    },
+		canvasContainer: {
+			width: '50%',
+      height: '50%',
+      backgroundColor: '#ffffff',
+      borderRadius: 10,
+      overflow: 'hidden',
+      elevation: 1,
+      alignSelf: 'center'
+		},
+		drawingBoard: {
+			height: '100%',
+			width: '100%',
+			zIndex: 10
+		},
+		canvas: {
+			height: '100%',
+      width: '100%',
+      position: 'absolute'
+		}
 });
